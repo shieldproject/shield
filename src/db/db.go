d18701f886863c29088d6b8cea4982caf7bd5216
James Hunt <james@niftylogic.com>
2015-10-29 10:35:44 -0400

    Initial DB layer

---
+++ src/db/db.go
@@ -0,0 +1,125 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Query struct {
+	Raw    string
+	Cooked *sql.Stmt
+}
+
+type DB struct {
+	connection *sql.DB
+	Driver     string
+	DSN        string
+
+	qCache map[string]*Query
+}
+
+// Are we connected?
+func (db *DB) Connected() bool {
+	if db.connection == nil {
+		return false
+	}
+	return true
+}
+
+// Connect to the backend database
+func (db *DB) Connect() error {
+	connection, err := sql.Open(db.Driver, db.DSN)
+	if err != nil {
+		return err
+	}
+
+	db.connection = connection
+	if db.qCache == nil {
+		db.qCache = make(map[string]*Query)
+	}
+	return nil
+}
+
+// Disconnect from the backend database
+func (db *DB) Disconnect() error {
+	if db.connection != nil {
+		if err := db.connection.Close(); err != nil {
+			return err
+		}
+		db.connection = nil
+
+		for _, q := range db.qCache {
+			q.Cooked = nil
+		}
+	}
+	return nil
+}
+
+// Register a SQL query
+func (db *DB) Cache(name string, sql string) error {
+	db.qCache[name] = &Query{Raw: sql}
+	return nil
+}
+
+// Is a SQL query cached?
+func (db *DB) Cached(name string) bool {
+	_, ok := db.qCache[name]
+	return ok
+}
+
+// Execute a named, non-data query (INSERT, UPDATE, DELETE, etc.)
+func (db *DB) Exec(name string, args ...interface{}) error {
+	s, err := db.statement(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.Exec(args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Execute a named, data query (SELECT)
+func (db *DB) Query(name string, args ...interface{}) (*sql.Rows, error) {
+	s, err := db.statement(name)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := s.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// Return the prepared Statement for a named query
+func (db *DB) statement(name string) (*sql.Stmt, error) {
+	if db.connection == nil {
+		return nil, fmt.Errorf("Not connected to database")
+	}
+
+	q, ok := db.qCache[name]
+	if !ok {
+		return nil, fmt.Errorf("Unknown query '%s'", name)
+	}
+
+	if q.Cooked == nil {
+		cooked, err := db.connection.Prepare(q.Raw)
+		if err != nil {
+			return nil, err
+		}
+
+		q.Cooked = cooked
+	}
+
+	if q.Cooked == nil { // still?
+		return nil, fmt.Errorf("Weird bug: query '%s' is still not properly prepared", name)
+	}
+
+	return q.Cooked, nil
+}
---
+++ src/db/db_test.go
@@ -0,0 +1,162 @@
+package db_test
+
+import (
+	. "db"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"database/sql"
+
+	// sql drivers
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var _ = Describe("Database", func() {
+	Describe("Connecting to the database", func() {
+		Context("With an invalid DSN", func() {
+			It("should fail", func() {
+				db := &DB{
+					Driver: "invalid",
+					DSN:    "does-not-matter",
+				}
+
+				Ω(db.Connect()).Should(HaveOccurred())
+				Ω(db.Connected()).Should(BeFalse())
+				Ω(db.Disconnect()).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("With an in-memory SQLite database", func() {
+			It("should succeed", func() {
+				db := &DB{
+					Driver: "sqlite3",
+					DSN:    ":memory:",
+				}
+
+				Ω(db.Connect()).ShouldNot(HaveOccurred())
+				Ω(db.Connected()).Should(BeTrue())
+				Ω(db.Disconnect()).ShouldNot(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Registering (cached) SQL queries", func() {
+		var db *DB
+
+		BeforeEach(func() {
+			db = &DB{
+				Driver: "sqlite3",
+				DSN:    ":memory:",
+			}
+
+			Ω(db.Connect()).ShouldNot(HaveOccurred())
+			Ω(db.Connected()).Should(BeTrue())
+		})
+
+		Context("With an empty query cache", func() {
+			It("has nothing registered", func() {
+				Ω(db.Cached("my-query")).Should(BeFalse())
+			})
+
+			It("can register a SQL command", func() {
+				Ω(db.Cache("my-query", "SELECT * FROM table")).ShouldNot(HaveOccurred())
+				Ω(db.Cached("my-query")).Should(BeTrue())
+			})
+
+			It("can register the same name multiple times", func() {
+				Ω(db.Cache("my-query", "SELECT * FROM table")).ShouldNot(HaveOccurred())
+				Ω(db.Cache("my-query", "SELECT * FROM other_table")).ShouldNot(HaveOccurred())
+				Ω(db.Cached("my-query")).Should(BeTrue())
+			})
+		})
+	})
+
+	Describe("Running SQL queries", func() {
+		var db *DB
+
+		BeforeEach(func() {
+			db = &DB{
+				Driver: "sqlite3",
+				DSN:    ":memory:",
+			}
+
+			Ω(db.Connect()).ShouldNot(HaveOccurred())
+
+			Ω(db.Cache("schema", `CREATE TABLE things (type TEXT, number INTEGER)`)).
+				ShouldNot(HaveOccurred())
+			Ω(db.Cache("new-thing", `INSERT INTO things (type, number) VALUES (?, 0)`)).
+				ShouldNot(HaveOccurred())
+			Ω(db.Cache("increase", `UPDATE things SET number = number + ? WHERE type = ?`)).
+				ShouldNot(HaveOccurred())
+			Ω(db.Cache("how-many?", `SELECT number FROM things WHERE type = ?`)).
+				ShouldNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			db.Disconnect()
+		})
+
+		Context("With an empty database", func() {
+			It("can create tables", func() {
+				Ω(db.Exec("schema")).ShouldNot(HaveOccurred())
+			})
+		})
+
+		numberOfThingsIn := func(r *sql.Rows) int {
+			var n int
+
+			Ω(r).ShouldNot(BeNil())
+			Ω(r.Next()).Should(BeTrue())
+			Ω(r.Scan(&n)).ShouldNot(HaveOccurred())
+			return n
+		}
+
+		Context("With an empty table", func() {
+			BeforeEach(func() {
+				db.Disconnect()
+				Ω(db.Connect()).ShouldNot(HaveOccurred())
+
+				Ω(db.Exec("schema")).ShouldNot(HaveOccurred())
+			})
+
+			It("can insert records", func() {
+				Ω(db.Exec("new-thing", "monkey")).ShouldNot(HaveOccurred())
+
+				r, err := db.Query("how-many?", "monkey")
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(numberOfThingsIn(r)).Should(Equal(0))
+			})
+
+			It("can update records", func() {
+				Ω(db.Exec("new-thing", "monkey")).ShouldNot(HaveOccurred())
+				Ω(db.Exec("increase", 42, "monkey")).ShouldNot(HaveOccurred())
+
+				r, err := db.Query("how-many?", "monkey")
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(numberOfThingsIn(r)).Should(Equal(42))
+			})
+
+			It("raises an error if an uncached query is requested for Exec()", func() {
+				Ω(db.Exec("not-a-query")).Should(HaveOccurred())
+			})
+
+			It("raises an error if an uncached query is requested for Query()", func() {
+				r, err := db.Query("not-a-query")
+				Ω(r).Should(BeNil())
+				Ω(err).Should(HaveOccurred())
+			})
+		})
+
+		Context("With malformed SQL queries", func() {
+			BeforeEach(func() {
+				Ω(db.Cache("error", `DO STUFF IN SQL`)).
+					ShouldNot(HaveOccurred())
+			})
+
+			It("propagates errors from sql driver", func() {
+				Ω(db.Exec("errors")).Should(HaveOccurred())
+			})
+		})
+	})
+})
