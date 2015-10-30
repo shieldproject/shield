f206e79e854e3d7581b1fe1bb3ba23ce3f36437f
James Hunt <james@niftylogic.com>
2015-10-29 21:34:33 -0400

    Start of an ORM

---
+++ src/db/orm.go
@@ -0,0 +1,192 @@
+package db
+
+import (
+	"fmt"
+)
+
+type ORM struct {
+	db *DB
+}
+
+func NewORM(db *DB) (*ORM, error) {
+	if db == nil {
+		return nil, fmt.Errorf("No database given to NewORM()")
+	}
+	if !db.Connected() {
+		return nil, fmt.Errorf("Not connected to database yet")
+	}
+
+	return &ORM{db: db}, nil
+}
+
+func (o *ORM) Setup() error {
+	v, err := o.schemaVersion()
+	if err != nil {
+		return err
+	}
+
+	if v == 0 {
+		err = o.v1schema()
+	} else {
+		err = fmt.Errorf("Schema version %d is newer than this version of SHIELD", v)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	/* FIXME: cache queries we're going to need */
+
+	return nil
+}
+
+func (o *ORM) schemaVersion() (uint, error) {
+	err := o.db.Cache("schema:version", `SELECT version FROM schema_info LIMIT 1`)
+	if err != nil {
+		return 0, err
+	}
+
+	r, err := o.db.Query("schema:version")
+	// failed query = no schema
+	// FIXME: better error object introspection?
+	if err != nil {
+		return 0, nil
+	}
+
+	// no records = no schema
+	if !r.Next() {
+		return 0, nil
+	}
+
+	var v int
+	err = r.Scan(&v)
+	// failed unmarshall is an actual error
+	if err != nil {
+		return 0, err
+	}
+
+	// invalid (negative) schema version is an actual error
+	if v < 0 {
+		return 0, fmt.Errorf("Invalid schema version %d found", v)
+	}
+
+	return uint(v), nil
+}
+
+func (o *ORM) v1schema() error {
+	o.db.ExecOnce(`CREATE TABLE schema_info (
+                              version INTEGER
+                  )`)
+	o.db.ExecOnce(`INSERT INTO schema_info VALUES (1)`)
+
+	o.db.ExecOnce(`CREATE TABLE targets (
+                    uuid      UUID PRIMARY KEY,
+                    name      TEXT,
+                    summary   TEXT,
+                    plugin    TEXT,
+                    endpoint  TEXT
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE stores (
+                    uuid      UUID PRIMARY KEY,
+                    name      TEXT,
+                    summary   TEXT,
+                    plugin    TEXT,
+                    endpoint  TEXT
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE schedules (
+                    uuid      UUID PRIMARY KEY,
+                    name      TEXT,
+                    summary   TEXT,
+                    timespec  TEXT
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE retention (
+                    uuid     UUID PRIMARY KEY,
+                    name     TEXT,
+                    summary  TEXT,
+                    expiry   INTEGER
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE jobs (
+                    uuid            UUID PRIMARY KEY,
+                    target_uuid     UUID,
+                    store_uuid      UUID,
+                    schedule_uuid   UUID,
+                    retention_uuid  UUID,
+                    paused          BOOLEAN,
+                    name            TEXT,
+                    summary         TEXT
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE archives (
+                    uuid         UUID PRIMARY KEY,
+                    target_uuid  UUID,
+                    store_uuid   UUID,
+                    store_key    TEXT,
+
+                    taken_at     timestamp without time zone,
+                    expires_at   timestamp without time zone,
+                    notes        TEXT
+                  )`)
+
+	o.db.ExecOnce(`CREATE TABLE tasks (
+                    uuid      UUID PRIMARY KEY,
+                    owner     TEXT,
+                    op        TEXT,
+                    args      TEXT,
+
+                    job_uuid      UUID,
+                    archive_uuid  UUID,
+
+                    status      status,
+                    started_at  timestamp without time zone,
+                    stopped_at  timestamp without time zone,
+
+                    log       TEXT,
+                    debug     TEXT
+                  )`)
+
+	return nil
+}
+
+/* FIXME: determine what ORM layers we need */
+
+// func annotateNameAndSummary(q string, id uuid.UUID, name string, summary string) error
+// func AnnotateArchive(id uuid.UUID, notes string) error
+// func AnnotateJob(id uuid.UUID, name string, summary string) error
+// func AnnotateRetentionPolicy(id uuid.UUID, name string, summary string) error
+// func AnnotateSchedule(id uuid.UUID, name string, summary string) error
+// func AnnotateStore(id uuid.UUID, name string, summary string) error
+// func AnnotateTarget(id uuid.UUID, name string, summary string) error
+// func AnnotateTask(id uuid.UUID, owner string) error
+
+// func CreateTarget(plugin string, endpoint interface{}) (uuid.UUID, error)
+// func UpdateTarget(id uuid.UUID, plugin string, endpoint interface{}) error
+// func DeleteTarget(id uuid.UUID) error
+
+// func CreateStore(plugin string, endpoint interface{}) (uuid.UUID, error)
+// func UpdateStore(id uuid.UUID, plugin string, endpoint interface{}) error
+// func DeleteStore(id uuid.UUID) error
+
+// func CreateSchedule(timespec string) (uuid.UUID, error)
+// func UpdateSchedule(id uuid.UUID, timespec string) error
+// func DeleteSchedule(id uuid.UUID)
+
+// func CreateRetentionPolicy(expiry uint) (uuid.UUID, error)
+// func UpdateRetentionPolicy(id uuid.UUID, expiry uint) error
+// func DeleteRetentionPolicy(id uuid.UUID)
+
+// func CreateJob(target uuid.UUID, store uuid.UUID, schedule uuid.UUID, retention uuid.UUID) (uuid.UUID, error)
+// func PauseJob(id uuid.UUID) error
+// func UnpauseJob(id uuid.UUID) error
+// func DeleteJob(id uuid.UUID) error
+
+// func CreateArchive(job uuid.UUID, key string) (id uuid.UUID, error)
+// func DeleteArchive(id uuid.UUID) error
+
+// func CreateTask(op string, args string, job uuid.UUID) (uuid.UUID, error)
+// func CompleteTask(id uuid.UUID) error
+// func CancelTask(id uuid.UUID) error
+// func UpdateTaskLog(id uuid.UUID, log string) error
---
+++ src/db/orm_test.go
@@ -0,0 +1,98 @@
+package db_test
+
+import (
+	"fmt"
+
+	. "db"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	// sql drivers
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var _ = Describe("ORM", func() {
+	Describe("Initializing the schema", func() {
+		Context("With a new database", func() {
+			var db *DB
+
+			BeforeEach(func() {
+				db = &DB{
+					Driver: "sqlite3",
+					DSN:    ":memory:",
+				}
+
+				Ω(db.Connect()).ShouldNot(HaveOccurred())
+				Ω(db.Connected()).Should(BeTrue())
+			})
+
+			It("should succeed", func() {
+				orm, err := NewORM(db)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(orm).ShouldNot(BeNil())
+			})
+
+			It("should not create tables until Setup() is called", func() {
+				orm, err := NewORM(db)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(orm).ShouldNot(BeNil())
+
+				Ω(db.ExecOnce("SELECT * FROM schema_info")).
+					Should(HaveOccurred())
+			})
+
+			It("should create tables during Setup()", func() {
+				orm, err := NewORM(db)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(orm).ShouldNot(BeNil())
+
+				Ω(orm.Setup()).ShouldNot(HaveOccurred())
+
+				Ω(db.ExecOnce("SELECT * FROM schema_info")).
+					ShouldNot(HaveOccurred())
+			})
+
+			It("should set the version number in schema_info", func() {
+				orm, err := NewORM(db)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(orm).ShouldNot(BeNil())
+
+				Ω(orm.Setup()).ShouldNot(HaveOccurred())
+
+				Ω(db.Cache("schema-version", `SELECT version FROM schema_info`)).
+					ShouldNot(HaveOccurred())
+
+				r, err := db.Query("schema-version")
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(r).ShouldNot(BeNil())
+				Ω(r.Next()).Should(BeTrue())
+
+				var v int
+				Ω(r.Scan(&v)).ShouldNot(HaveOccurred())
+				Ω(v).Should(Equal(1))
+			})
+
+			It("creates the correct tables", func() {
+				orm, err := NewORM(db)
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(orm).ShouldNot(BeNil())
+
+				Ω(orm.Setup()).ShouldNot(HaveOccurred())
+
+				tableExists := func(table string) {
+					sql := fmt.Sprintf("SELECT * FROM %s", table)
+					Ω(db.ExecOnce(sql)).ShouldNot(HaveOccurred())
+				}
+
+				tableExists("targets")
+				tableExists("stores")
+				tableExists("schedules")
+				tableExists("retention")
+				tableExists("jobs")
+				tableExists("archives")
+				tableExists("tasks")
+			})
+		})
+	})
+})
