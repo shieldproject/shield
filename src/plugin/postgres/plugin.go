3853c2c31ae5665368f1fcadf881ae0137b5cfa2
Geoff Franks <geoff@starkandwayne.com>
2015-11-10 16:26:32 -0500

    Refactored postgres plugin

--- src/plugin/postgres/plugin.go
+++ src/plugin/postgres/plugin.go
@@ -22,89 +22,38 @@ func main() {
 
 type PostgresPlugin plugin.PluginInfo
 
+type PostgresConnectionInfo struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DB       string
+	DumpArgs string
+	Bin      string
+}
+
 func (p PostgresPlugin) Meta() plugin.PluginInfo {
 	return plugin.PluginInfo(p)
 }
 
 func (p PostgresPlugin) Backup(endpoint plugin.ShieldEndpoint) error {
-	pgUser, err := endpoint.StringValue("pg_user")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGUSER", pgUser)
-
-	pgPass, err := endpoint.StringValue("pg_password")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGPASSWORD", pgPass)
-
-	pgHost, err := endpoint.StringValue("pg_host")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGHOST", pgHost)
-
-	pgPort, err := endpoint.StringValue("pg_port")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGPORT", pgPort)
-
-	pgDB, err := endpoint.StringValue("pg_database")
+	pg, err := pgConnectionInfo(endpoint)
 	if err != nil {
 		return err
 	}
 
-	pgDumpBin, err := endpoint.StringValue("pg_dump")
-	if err != nil {
-		return err
-	}
-
-	pgDumpArgs, err := endpoint.StringValue("pg_dump_args")
-	if err != nil {
-		return err
-	}
-
-	return plugin.Exec(fmt.Sprintf("%s %s -cC --format p --no-password %s", pgDumpBin, pgDumpArgs, pgDB), plugin.STDOUT)
+	setupEnvironmentVariables(pg)
+	return plugin.Exec(fmt.Sprintf("%s/pg_dump %s -cC --format p --no-password %s", pg.Bin, pg.DumpArgs, pg.DB), plugin.STDOUT)
 }
 
 func (p PostgresPlugin) Restore(endpoint plugin.ShieldEndpoint) error {
-	pgUser, err := endpoint.StringValue("pg_user")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGUSER", pgUser)
-
-	pgPass, err := endpoint.StringValue("pg_password")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGPASSWORD", pgPass)
-
-	pgHost, err := endpoint.StringValue("pg_host")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGHOST", pgHost)
-
-	pgPort, err := endpoint.StringValue("pg_port")
-	if err != nil {
-		return err
-	}
-	os.Setenv("PGPORT", pgPort)
-
-	pgDB, err := endpoint.StringValue("pg_database")
+	pg, err := pgConnectionInfo(endpoint)
 	if err != nil {
 		return err
 	}
 
-	pgBin, err := endpoint.StringValue("pg_psql")
-	if err != nil {
-		return err
-	}
-
-	return plugin.Exec(fmt.Sprintf("%s -d %s", pgBin, pgDB), plugin.STDIN)
+	setupEnvironmentVariables(pg)
+	return plugin.Exec(fmt.Sprintf("%s/psql -d %s", pg.Bin, pg.DB), plugin.STDIN)
 }
 
 func (p PostgresPlugin) Store(endpoint plugin.ShieldEndpoint) (string, error) {
@@ -117,4 +66,58 @@ func (p PostgresPlugin) Retrieve(endpoint plugin.ShieldEndpoint, file string) er
 
 func (p PostgresPlugin) Purge(endpoint plugin.ShieldEndpoint, file string) error {
 	return plugin.UNIMPLEMENTED
+}
+
+func setupEnvironmentVariables(pg *PostgresConnectionInfo) {
+	os.Setenv("PGUSER", pg.User)
+	os.Setenv("PGPASSWORD", pg.Password)
+	os.Setenv("PGHOST", pg.Host)
+	os.Setenv("PGPORT", pg.Port)
+}
+
+func pgConnectionInfo(endpoint plugin.ShieldEndpoint) (*PostgresConnectionInfo, error) {
+	user, err := endpoint.StringValue("pg_user")
+	if err != nil {
+		return nil, err
+	}
+
+	password, err := endpoint.StringValue("pg_password")
+	if err != nil {
+		return nil, err
+	}
+
+	host, err := endpoint.StringValue("pg_host")
+	if err != nil {
+		return nil, err
+	}
+
+	port, err := endpoint.StringValue("pg_port")
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := endpoint.StringValue("pg_database")
+	if err != nil {
+		return nil, err
+	}
+
+	bin, err := endpoint.StringValue("pg_dump")
+	if err != nil {
+		return nil, err
+	}
+
+	dumpArgs, err := endpoint.StringValue("pg_dump_args")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresConnectionInfo{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DB:       db,
+		DumpArgs: dumpArgs,
+		Bin:      bin,
+	}, nil
 }
