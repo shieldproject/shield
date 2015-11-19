6ac7b61176a9e9ce6c5e5125f84855a89a811a6f
Geoff Franks <geoff@starkandwayne.com>
2015-11-19 14:22:23 -0500

    Implemented redis backup/restore functionality

--- plugin/redis/plugin.go
+++ plugin/redis/plugin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/starkandwayne/shield/plugin"
+	"time"
 )
 
 func main() {
@@ -25,11 +26,54 @@ func (p RedisPlugin) Meta() plugin.PluginInfo {
 }
 
 func (p RedisPlugin) Backup(endpoint plugin.ShieldEndpoint) error {
-	return plugin.UNIMPLEMENTED
+	err := plugin.Exec("tar -c -C /var/vcap/store .", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (p RedisPlugin) Restore(endpoint plugin.ShieldEndpoint) error {
-	return plugin.UNIMPLEMENTED
+	err := plugin.Exec("/var/vcap/bosh/bin/monit stop all", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	err = plugin.Exec("/bin/bash -c \"while [[ $(/var/vcap/bosh/bin/monit summary | /bin/grep running) ]]; do /bin/sleep 1; done\"", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	err = plugin.Exec("/bin/pkill redis-server", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(2 * time.Second)
+
+	err = plugin.Exec("/bin/pkill -9 redis-server", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(1 * time.Second)
+
+	err = plugin.Exec("tar -x -C /var/vcap/store . ", plugin.STDIN)
+	if err != nil {
+		return err
+	}
+
+	err = plugin.Exec("/bin/bash -c '/usr/bin/yes | /usr/bin/find /var/vcap/store -name appendonly.aof -exec /var/vcap/packages/redis/bin/redis-check-aof --fix {} \\;'", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+
+	err = plugin.Exec("/var/vcap/bosh/bin/monit start all", plugin.STDOUT)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (p RedisPlugin) Store(endpoint plugin.ShieldEndpoint) (string, error) {
