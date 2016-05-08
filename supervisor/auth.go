561b3e7ad30a801ced8c2c54d0f8250e2369d62d
James Hunt <james@niftylogic.com>
2016-05-08 15:32:32 -0400

    More debugging diagnostics for authentication

--- supervisor/auth.go
+++ supervisor/auth.go
@@ -7,6 +7,10 @@ import (
 	"strings"
 )
 
+func obfuscate(p string) string {
+	return strings.Repeat("•", len(p))
+}
+
 type Authenticator interface {
 	IsAuthenticated(*http.Request) bool
 	RequireAuth(http.ResponseWriter, *http.Request)
@@ -20,17 +24,28 @@ type BasicAuthenticator struct {
 
 func (ba BasicAuthenticator) IsAuthenticated(r *http.Request) bool {
 	authType, authToken := AuthHeader(r)
+	log.Debugf("Checking `Authorization: %s %s` against our configuration", authType, authToken)
+
 	if strings.ToLower(authType) == "basic" {
 		decoded, err := base64.StdEncoding.DecodeString(authToken)
 		if err != nil {
-			log.Infof("Received invalid auth request")
-		} else {
-			creds := strings.SplitN(string(decoded), ":", 2)
-			if creds[0] == ba.Cfg.User && creds[1] == ba.Cfg.Password {
-				return true
-			}
+			log.Infof("Authorization header is corrupt: %s", err)
+			return false
 		}
+
+		creds := strings.SplitN(string(decoded), ":", 2)
+		if len(creds) != 2 {
+			log.Infof("Authorization header is corrupt: '%s' does not contain a ':' delimiter",
+				string(decoded))
+			return false
+		}
+
+		log.Debugf("Received Authorization credentials for user '%s', password '%s'", creds[0], obfuscate(creds[1]))
+		log.Debugf("checking against the configured credentials '%s', password '%s'", ba.Cfg.User, obfuscate(ba.Cfg.Password))
+		return creds[0] == ba.Cfg.User && creds[1] == ba.Cfg.Password
 	}
+
+	log.Infof("Received an invalid Authorization header type '%s' (not 'Basic')", authType)
 	return false
 }
 
@@ -67,6 +82,7 @@ func Authenticate(tokens map[string]string, next http.Handler) http.Handler {
 }
 
 func AuthHeader(r *http.Request) (string, string) {
+	log.Debugf("Retrieving auth header `%v`", r.Header.Get("Authorization"))
 	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 	if len(auth) != 2 {
 		return "", ""
