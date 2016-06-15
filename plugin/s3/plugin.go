3c2c7f883440ccdc5c7410cdd820581aa560b39a
Geoff Franks <geoff.franks@gmail.com>
2016-06-15 13:53:48 -0400

    drop trailing comma in json docs

--- plugin/s3/plugin.go
+++ plugin/s3/plugin.go
@@ -26,7 +26,7 @@
 //        "bucket":              "bucket-name",
 //        "prefix":              "/path/inside/bucket/to/place/backup/data",
 //        "signature_version":   "4",  # should be 2 or 4. Defaults to 4
-//        "socks5_proxy":        "",   # optionally defined SOCKS5 proxy to use for the s3 communications
+//        "socks5_proxy":        ""    # optionally defined SOCKS5 proxy to use for the s3 communications
 //    }
 //
 // `prefix` will default to the empty string, and backups will be placed in the
