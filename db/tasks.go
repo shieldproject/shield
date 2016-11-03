shieldproject/shield

commit fb2fcc5951b274e3226215fe096525e8676cbc1e

allow only a single task per target
Since task scheduling is not done in parallel, we can check prior to
starting a task whether it is allowed to run that task.

A task may not run if a task with same target uuid is running. This is
useful if a restore operation is currently in progress and another
backup should be initiated.

db/tasks.go
@@ -247,6 +247,21 @@ func (db *DB) CreatePurgeTask(owner string, archive *Archive, agent string) (*Ta
 	return db.GetTask(id)
 }
 
+func (db *DB) IsTaskRunnable(task *Task) (bool, error) {
+	r, err := db.Query(`
+		SELECT uuid FROM tasks
+		  WHERE target_uuid = ? AND status = ? LIMIT 1`, task.TargetUUID.String(), RunningStatus)
+	if err != nil {
+		return false, err
+	}
+	defer r.Close()
+
+	if !r.Next() {
+		return true, nil
+	}
+	return false, nil
+}
+
 func (db *DB) StartTask(id uuid.UUID, effective time.Time) error {
 	validtime := ValidateEffectiveUnix(effective)
 	return db.Exec(

supervisor/supervisor.go
@@ -294,18 +294,24 @@ func (s *Supervisor) Run() error {
 			}
 
 			// see if we have anything in the schedule queue
-		SchedQueue:
-			for len(s.schedq) > 0 {
+			for i, t := range s.schedq {
+				runnable, err := s.Database.IsTaskRunnable(t)
+				if err != nil {
+					continue
+				}
+				if !runnable {
+					continue
+				}
+
 				select {
-				case s.workers <- s.schedq[0]:
-					s.Database.StartTask(s.schedq[0].UUID, time.Now())
-					s.schedq[0].Attempts++
+				case s.workers <- t:
+					s.Database.StartTask(t.UUID, time.Now())
+					s.schedq[i].Attempts++
 					log.Infof("sent a task to a worker")
-					s.runq = append(s.runq, s.schedq[0])
+					s.runq = append(s.runq, s.schedq[i])
 					log.Debugf("added task to the runq")
-					s.schedq = s.schedq[1:]
+					s.schedq = append(s.schedq[:i], s.schedq[i+1:]...)
 				default:
-					break SchedQueue
 				}
 			}
 

db/tasks_test.go
@@ -368,4 +368,30 @@ var _ = Describe("Task Management", func() {
 			}))
 		})
 	})
+
+	Describe("IsTaskRunnable", func() {
+		notRunningTaskTargetUUID := uuid.NewRandom()
+		runningTaskTargetUUID := uuid.NewRandom()
+
+		BeforeEach(func() {
+			err := db.Exec(fmt.Sprintf(`INSERT INTO tasks (uuid, op, status, requested_at, target_uuid)`+
+				`VALUES('%s', '%s', '%s', %d, '%s')`,
+				uuid.NewRandom().String(), BackupOperation, PendingStatus, 0, notRunningTaskTargetUUID.String()))
+			Expect(err).ShouldNot(HaveOccurred())
+			err = db.Exec(fmt.Sprintf(`INSERT INTO tasks (uuid, op, status, requested_at, target_uuid)`+
+				`VALUES('%s', '%s', '%s', %d, '%s')`,
+				uuid.NewRandom().String(), BackupOperation, RunningStatus, 0, runningTaskTargetUUID.String()))
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+		It("Returns true if no other task with same target uuid is running", func() {
+			runnable, err := db.IsTaskRunnable(&Task{TargetUUID: notRunningTaskTargetUUID})
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(runnable).To(BeTrue())
+		})
+		It("Returns false if another task with same target uuid is running", func() {
+			runnable, err := db.IsTaskRunnable(&Task{TargetUUID: runningTaskTargetUUID})
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(runnable).To(BeFalse())
+		})
+	})
 })

commit 76e471c76b31c49ae993899a841b48402c97a846

Tasks without targets should *always* be runnable
Commit fb2fcc59 introduced a regression that caused "purge" tasks to
never be considered runnable, thanks to a bug in the SQL that probably
doesn't manifest on MySQL.  Go multiple DB backend support!

We now treat any task that doesn't have an explicit target as always
runnable, since they can't possible cause data corruption issues the way
a conccurrent backup + restore from/to the same target can.

Fixes #210, for real.

db/tasks.go
@@ -251,6 +251,10 @@ func (db *DB) CreatePurgeTask(owner string, archive *Archive, agent string) (*Ta
 }
 
 func (db *DB) IsTaskRunnable(task *Task) (bool, error) {
+	/* tasks without targets (i.e. purge tasks) are always valid */
+	if task.TargetUUID.String() == "" {
+		return true, nil
+	}
 	r, err := db.Query(`
 		SELECT uuid FROM tasks
 		  WHERE target_uuid = ? AND status = ? LIMIT 1`, task.TargetUUID.String(), RunningStatus)

supervisor/supervisor.go
@@ -300,9 +300,11 @@ func (s *Supervisor) Run() error {
 
 				runnable, err := s.Database.IsTaskRunnable(t)
 				if err != nil {
+					log.Errorf("failed to determine if task %s was runnable: %s", t.UUID, err)
 					continue
 				}
 				if !runnable {
+					log.Infof("skipping task %s; not runnable", t.UUID);
 					continue
 				}
 