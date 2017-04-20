e92674f3533c328b31c659e08ad4634e5b326c6c
Christopher Weibel <christopher.weibel@gmail.com>
2017-04-20 11:41:39 -0400

    Enhancements
     - An API endpoint `v1/status/jobs` was added to show the most recent success or failure of a job task.  Intended for a monitoring/alerting application to scrape this information periodically
     - In GUI, under Jobs the status is now color coded
     - Also on the dashboard recently failed jobs will appear in a notification bar at the top

--- db/jobs.go
+++ db/jobs.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/pborman/uuid"
+	. "github.com/starkandwayne/goutils/timestamp"
+
 	"github.com/starkandwayne/shield/timespec"
 )
 
@@ -29,6 +32,8 @@ type Job struct {
 	TargetPlugin   string    `json:"target_plugin"`
 	TargetEndpoint string    `json:"target_endpoint"`
 	Agent          string    `json:"agent"`
+	LastRun        Timestamp `json:"last_run"`
+	LastTaskStatus string    `json:"last_task_status"`
 
 	Spec    *timespec.Spec `json:"-"`
 	NextRun time.Time      `json:"-"`
@@ -48,11 +53,11 @@ type JobFilter struct {
 }
 
 func (f *JobFilter) Query() (string, []interface{}) {
-	wheres := []string{"j.uuid = j.uuid"}
+	var wheres []string = []string{"j.uuid = j.uuid"}
 	var args []interface{}
 	if f.SearchName != "" {
-		comparator := "LIKE"
-		toAdd := Pattern(f.SearchName)
+		var comparator string = "LIKE"
+		var toAdd string = Pattern(f.SearchName)
 		if f.ExactMatch {
 			comparator = "="
 			toAdd = f.SearchName
@@ -61,23 +66,23 @@ func (f *JobFilter) Query() (string, []interface{}) {
 		args = append(args, toAdd)
 	}
 	if f.ForTarget != "" {
-		wheres = append(wheres, "target_uuid = ?")
+		wheres = append(wheres, "j.target_uuid = ?")
 		args = append(args, f.ForTarget)
 	}
 	if f.ForStore != "" {
-		wheres = append(wheres, "store_uuid = ?")
+		wheres = append(wheres, "j.store_uuid = ?")
 		args = append(args, f.ForStore)
 	}
 	if f.ForSchedule != "" {
-		wheres = append(wheres, "schedule_uuid = ?")
+		wheres = append(wheres, "j.schedule_uuid = ?")
 		args = append(args, f.ForSchedule)
 	}
 	if f.ForRetention != "" {
-		wheres = append(wheres, "retention_uuid = ?")
+		wheres = append(wheres, "j.retention_uuid = ?")
 		args = append(args, f.ForRetention)
 	}
 	if f.SkipPaused || f.SkipUnpaused {
-		wheres = append(wheres, "paused = ?")
+		wheres = append(wheres, "j.paused = ?")
 		if f.SkipPaused {
 			args = append(args, 0)
 		} else {
@@ -86,17 +91,25 @@ func (f *JobFilter) Query() (string, []interface{}) {
 	}
 
 	return `
+	WITH most_recent_job_task_at AS (
+		SELECT job_uuid,  max(requested_at) AS requested_at FROM tasks group by job_uuid
+	),
+	most_recent_job_task AS (
+		SELECT t.started_at, t.status, t.job_uuid, t.uuid FROM tasks t, most_recent_job_task_at mr WHERE t.job_uuid = mr.job_uuid AND t.requested_at = mr.requested_at
+	)
 		SELECT j.uuid, j.name, j.summary, j.paused,
 		       r.name, r.uuid, r.expiry,
 		       sc.name, sc.uuid, sc.timespec,
 		       s.uuid, s.name, s.plugin, s.endpoint,
-		       t.uuid, t.name, t.plugin, t.endpoint, t.agent
+		       t.uuid, t.name, t.plugin, t.endpoint, t.agent,
+		       k.started_at, k.status
 
 			FROM jobs j
 				INNER JOIN retention  r  ON  r.uuid = j.retention_uuid
 				INNER JOIN schedules sc  ON sc.uuid = j.schedule_uuid
 				INNER JOIN stores     s  ON  s.uuid = j.store_uuid
 				INNER JOIN targets    t  ON  t.uuid = j.target_uuid
+				LEFT  JOIN most_recent_job_task k  ON  j.uuid = k.job_uuid
 
 			WHERE ` + strings.Join(wheres, " AND ") + `
 			ORDER BY j.name, j.uuid ASC
@@ -118,14 +131,18 @@ func (db *DB) GetAllJobs(filter *JobFilter) ([]*Job, error) {
 
 	for r.Next() {
 		ann := &Job{}
-		var this, retention, schedule, store, target NullUUID
+		var (
+			this, retention, schedule, store, target NullUUID
+			last_run                                 *int64
+			last_task_status                         sql.NullString
+		)
 		if err = r.Scan(
 			&this, &ann.Name, &ann.Summary, &ann.Paused,
 			&ann.RetentionName, &retention, &ann.Expiry,
 			&ann.ScheduleName, &schedule, &ann.ScheduleWhen,
 			&store, &ann.StoreName, &ann.StorePlugin, &ann.StoreEndpoint,
 			&target, &ann.TargetName, &ann.TargetPlugin, &ann.TargetEndpoint,
-			&ann.Agent); err != nil {
+			&ann.Agent, &last_run, &last_task_status); err != nil {
 			return l, err
 		}
 		ann.UUID = this.UUID
@@ -133,6 +150,90 @@ func (db *DB) GetAllJobs(filter *JobFilter) ([]*Job, error) {
 		ann.ScheduleUUID = schedule.UUID
 		ann.StoreUUID = store.UUID
 		ann.TargetUUID = target.UUID
+		if last_run != nil {
+			ann.LastRun = parseEpochTime(*last_run)
+			if last_task_status.Valid {
+				ann.LastTaskStatus = last_task_status.String
+			}
+		} else {
+			ann.LastRun = Timestamp{}
+			ann.LastTaskStatus = "pending"
+		}
+
+		l = append(l, ann)
+	}
+
+	return l, nil
+}
+
+func (db *DB) GetAllJobsStatus(filter *JobFilter) ([]*Job, error) {
+	if filter == nil {
+		filter = &JobFilter{}
+	}
+
+	l := []*Job{}
+	query := `
+	WITH most_recent_job_task_at AS (
+	  SELECT job_uuid,  max(requested_at) AS requested_at FROM tasks group by job_uuid
+	),
+	most_recent_job_task AS (
+	  SELECT t.started_at, t.status, t.job_uuid, t.uuid FROM tasks t, most_recent_job_task_at mr WHERE t.job_uuid = mr.job_uuid AND t.requested_at = mr.requested_at
+	)
+	SELECT j.uuid, j.name, j.summary, j.paused,
+	       r.name, r.uuid, r.expiry,
+	       sc.name, sc.uuid, sc.timespec,
+	       s.uuid, s.name, s.plugin, s.endpoint,
+	       t.uuid, t.name, t.plugin, t.endpoint, t.agent,
+	       k.started_at, k.status
+
+	  FROM jobs j
+	    INNER JOIN retention            r  ON  r.uuid = j.retention_uuid
+	    INNER JOIN schedules           sc  ON sc.uuid = j.schedule_uuid
+	    INNER JOIN stores               s  ON  s.uuid = j.store_uuid
+	    INNER JOIN targets              t  ON  t.uuid = j.target_uuid
+	    LEFT  JOIN most_recent_job_task k  ON  j.uuid = k.job_uuid
+
+	  ORDER BY j.name, j.uuid ASC;
+		`
+
+	r, err := db.Query(query)
+	if err != nil {
+		return l, err
+	}
+	defer r.Close()
+
+	for r.Next() {
+		ann := &Job{}
+		var (
+			this, retention, schedule, store, target NullUUID
+			last_run                                 *int64
+			last_task_status                         sql.NullString
+		)
+
+		if err = r.Scan(
+			&this, &ann.Name, &ann.Summary, &ann.Paused,
+			&ann.RetentionName, &retention, &ann.Expiry,
+			&ann.ScheduleName, &schedule, &ann.ScheduleWhen,
+			&store, &ann.StoreName, &ann.StorePlugin, &ann.StoreEndpoint,
+			&target, &ann.TargetName, &ann.TargetPlugin, &ann.TargetEndpoint,
+			&ann.Agent, &last_run, &last_task_status); err != nil {
+			return l, err
+		}
+		ann.UUID = this.UUID
+		ann.RetentionUUID = retention.UUID
+		ann.ScheduleUUID = schedule.UUID
+		ann.StoreUUID = store.UUID
+		ann.TargetUUID = target.UUID
+
+		if last_run != nil {
+			ann.LastRun = parseEpochTime(*last_run)
+			if last_task_status.Valid {
+				ann.LastTaskStatus = last_task_status.String
+			}
+		} else {
+			ann.LastRun = Timestamp{}
+			ann.LastTaskStatus = "pending"
+		}
 
 		l = append(l, ann)
 	}
