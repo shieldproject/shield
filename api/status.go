dfa3fefe52bd419a855939c5d4b0afa04e86b5de
Ferran Rodenas <frodenas@gmail.com>
2017-05-09 17:06:05 +0200

    Add GetJobsStatus API method (#279)

--- api/status.go
+++ api/status.go
@@ -14,3 +14,23 @@ func GetStatus() (Status, error) {
 	var data Status
 	return data, uri.Get(&data)
 }
+
+type JobsStatus map[string]JobHealth
+
+type JobHealth struct {
+	Name    string `json:"name"`
+	LastRun int64  `json:"last_run"`
+	NextRun int64  `json:"next_run"`
+	Paused  bool   `json:"paused"`
+	Status  string `json:"status"`
+}
+
+func GetJobsStatus() (JobsStatus, error) {
+	uri, err := ShieldURI("/v1/status/jobs")
+	if err != nil {
+		return JobsStatus{}, err
+	}
+
+	var data JobsStatus
+	return data, uri.Get(&data)
+}
