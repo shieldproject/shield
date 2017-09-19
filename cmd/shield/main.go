cd65c0b14b2f727b6fbb320b362c95902c809d32
Thomas Mitchell <tmitchell@starkandwayne.com>
2017-09-19 19:52:01 -0400

    Removed deprecated command aliases (#318)
    * Removed deprecated command aliases
    
    * Remove deprecation warnings on aliases
    
    Also removed the impending deprecation message from the help and added
    a check to make sure we don't register duplicate aliases.

--- cmd/shield/main.go
+++ cmd/shield/main.go
@@ -110,7 +110,6 @@ func main() {
 		ansi.Fprintf(os.Stderr, "@R{unrecognized command `%s'}\n", cmdname)
 		os.Exit(1)
 	}
-	cmds.MaybeWarnDeprecation(cmdname, cmd)
 
 	currentBackend := config.Current()
 	// only check for backends + creds if we aren't manipulating backends/help
@@ -154,58 +153,55 @@ func main() {
 
 func addCommands() {
 	cmds.Add("help", info.Usage).AKA("usage", "commands")
-	cmds.Add("status", info.Status).AKA("stat")
+	cmds.Add("status", info.Status)
 
-	cmds.Add("backends", backends.List).AKA("list backends", "ls be")
+	cmds.Add("backends", backends.List)
 	cmds.Add("backend", backends.Use).AKA("use backend", "use-backend")
-	cmds.Add("create-backend", backends.Create).AKA("create backend", "c be", "update backend")
+	cmds.Add("create-backend", backends.Create).AKA("create backend")
 	cmds.Add("rename-backend", backends.Rename).AKA("rename backend")
 	cmds.Add("delete-backend", backends.Delete).AKA("delete backend")
 
-	cmds.Add("targets", targets.List).AKA("list targets", "ls targets")
-	cmds.Add("target", targets.Get).AKA("show target", "view target", "display target", "list target", "ls target")
-	cmds.Add("create-target", targets.Create).AKA("create target", "new target", "create new target", "make target", "c t", "add target")
-	cmds.Add("edit-target", targets.Edit).AKA("edit target", "update target")
-	cmds.Add("delete-target", targets.Delete).AKA("delete target", "remove target", "rm target")
+	cmds.Add("targets", targets.List)
+	cmds.Add("target", targets.Get)
+	cmds.Add("create-target", targets.Create).AKA("create target")
+	cmds.Add("edit-target", targets.Edit).AKA("edit target")
+	cmds.Add("delete-target", targets.Delete).AKA("delete target")
 
-	cmds.Add("stores", stores.List).AKA("list stores, ls stores")
-	cmds.Add("store", stores.Get).AKA("show store", "view store", "display store", "list store", "ls store")
-	cmds.Add("create-store", stores.Create).AKA("create store", "new store", "create new store", "make store", "c st")
-	cmds.Add("edit-store", stores.Edit).AKA("edit store", "update store")
-	cmds.Add("delete-store", stores.Delete).AKA("delete store", "remove store", "rm store")
+	cmds.Add("stores", stores.List)
+	cmds.Add("store", stores.Get)
+	cmds.Add("create-store", stores.Create).AKA("create store")
+	cmds.Add("edit-store", stores.Edit).AKA("edit store")
+	cmds.Add("delete-store", stores.Delete).AKA("delete store")
 
-	cmds.Add("schedules", schedules.List).AKA("list schedules", "ls schedules")
-	cmds.Add("schedule", schedules.Get).AKA("show schedule", "view schedule", "display schedule", "list schedule", "ls schedule")
-	cmds.Add("create-schedule", schedules.Create).AKA("create schedule", "new schedule", "create new schedule", "make schedule", "c s")
-	cmds.Add("edit-schedule", schedules.Edit).AKA("edit schedule", "update schedule")
-	cmds.Add("delete-schedule", schedules.Delete).AKA("delete schedule", "remove schedule", "rm schedule")
+	cmds.Add("schedules", schedules.List)
+	cmds.Add("schedule", schedules.Get)
+	cmds.Add("create-schedule", schedules.Create).AKA("create schedule")
+	cmds.Add("edit-schedule", schedules.Edit).AKA("edit schedule")
+	cmds.Add("delete-schedule", schedules.Delete).AKA("delete schedule")
 
-	cmds.Add("policies", policies.List).AKA("list retention policies", "ls retention policies", "list policies", "ls policies")
-	cmds.Add("policy", policies.Get).AKA("show retention policy", "view retention policy", "display retention policy",
-		"list retention policy", "show policy", "view policy", "display policy", "list policy")
-	cmds.Add("create-policy", policies.Create).AKA("create retention policy", "new retention policy", "create new retention policy",
-		"make retention policy", "create policy", "new policy", "create new policy", "make policy")
-	cmds.Add("edit-policy", policies.Edit).AKA("edit retention policy", "update retention policy", "edit policy", "update policy")
-	cmds.Add("delete-policy", policies.Delete).AKA("delete retention policy", "remove retention policy", "rm retention policy",
-		"delete policy", "remove policy", "rm policy")
+	cmds.Add("policies", policies.List).AKA("retention policies", "retention-policies")
+	cmds.Add("policy", policies.Get).AKA("retention policy", "retention-policy")
+	cmds.Add("create-policy", policies.Create).AKA("create retention policy", "create-retention-policy", "create policy")
+	cmds.Add("edit-policy", policies.Edit).AKA("edit retention policy", "edit policy", "edit-retention-policy")
+	cmds.Add("delete-policy", policies.Delete).AKA("delete retention policy", "delete policy", "delete-retention-policy")
 
-	cmds.Add("jobs", jobs.List).AKA("list jobs", "ls jobs", "ls j")
-	cmds.Add("job", jobs.Get).AKA("show job", "view job", "display job", "list job", "ls job")
-	cmds.Add("create-job", jobs.Create).AKA("create job", "new job", "create new job", "make job", "c j")
-	cmds.Add("edit-job", jobs.Edit).AKA("edit job", "update job")
-	cmds.Add("delete-job", jobs.Delete).AKA("delete job", "remove job", "rm job")
-	cmds.Add("pause", jobs.Pause).AKA("pause job")
-	cmds.Add("unpause", jobs.Unpause).AKA("unpause job")
-	cmds.Add("run", jobs.Run).AKA("run job")
+	cmds.Add("jobs", jobs.List)
+	cmds.Add("job", jobs.Get)
+	cmds.Add("create-job", jobs.Create).AKA("create job")
+	cmds.Add("edit-job", jobs.Edit).AKA("edit job")
+	cmds.Add("delete-job", jobs.Delete).AKA("delete job")
+	cmds.Add("pause", jobs.Pause).AKA("pause job", "pause-job")
+	cmds.Add("unpause", jobs.Unpause).AKA("unpause job", "unpause-job")
+	cmds.Add("run", jobs.Run).AKA("run job", "run-job")
 
-	cmds.Add("archives", archives.List).AKA("list archives", "ls archives")
-	cmds.Add("archive", archives.Get).AKA("show archive", "view archive", "display archive", "list archive", "ls archive")
+	cmds.Add("archives", archives.List)
+	cmds.Add("archive", archives.Get)
 	cmds.Add("restore", archives.Restore).AKA("restore archive", "restore-archive")
-	cmds.Add("delete-archive", archives.Delete).AKA("delete archive", "remove archive", "rm archive")
+	cmds.Add("delete-archive", archives.Delete).AKA("delete archive")
 
-	cmds.Add("tasks", tasks.List).AKA("list tasks", "ls tasks")
-	cmds.Add("task", tasks.Get).AKA("show task", "view task", "display task", "list task", "ls task")
-	cmds.Add("cancel", tasks.Cancel).AKA("cancel-task", "cancel task", "stop task")
+	cmds.Add("tasks", tasks.List)
+	cmds.Add("task", tasks.Get)
+	cmds.Add("cancel", tasks.Cancel).AKA("cancel-task", "cancel task")
 }
 
 func addGlobalFlags() {
