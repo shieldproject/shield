shieldproject/shield

commit d4ba545ec0292b797d797466d4c9a74d14b65c5d

Configuration Wizard
The #!/do/configure web UI page now walks the user through a
configuration wizard for setting up a new job, optionally allowing them
to create targets, stores, and retention policies along the way.

The "create" operations in the `/v2` endpoints for targets, stores, and
retention policies, picked up a new `?test=t` query string parameter for
running in _test mode_, where the actual thing will not be created, but
the validation will still be performed.  The Web UI needs this.

timespec/lang.y
@@ -11,13 +11,15 @@ import (
 	time    int
 	wday    time.Weekday
 	spec   *Spec
+	truth   bool
 }
 
 %type  <time>   time_in_MM
 %type  <time>   time_in_HHMM
-%type  <numval> am_or_pm month_day minutes
+%type  <numval> month_day minutes
 %type  <wday>   day_name
 %type  <spec>   spec hourly_spec daily_spec weekly_spec monthly_spec
+%type  <truth>  am_or_pm
 
 %token <numval> NUMBER ORDINAL
 
@@ -77,17 +79,17 @@ minutes: QUARTER { $$ = 15 }
        | NUMBER  { $$ = $1 }
        ;
 
-time_in_MM: anyhour ':' NUMBER  { $$ = hhmm(0, $3) }
-          |             NUMBER  { $$ = hhmm(0, $1) }
-          |     minutes AFTER   { $$ = hhmm(0, $1) }
-          |     minutes TIL     { $$ = hhmm(0, 60 - $1) }
+time_in_MM: anyhour ':' NUMBER  { $$ = hhmm24(0, $3) }
+          |             NUMBER  { $$ = hhmm24(0, $1) }
+          |     minutes AFTER   { $$ = hhmm24(0, $1) }
+          |     minutes TIL     { $$ = hhmm24(0, 60 - $1) }
           ;
 
-time_in_HHMM : NUMBER ':' NUMBER              { $$ = hhmm($1,      $3) }
-             | NUMBER ':' NUMBER am_or_pm     { $$ = hhmm($1 + $4, $3) }
-             | NUMBER ':' NUMBER ' ' am_or_pm { $$ = hhmm($1 + $5, $3) }
-             | NUMBER am_or_pm                { $$ = hhmm($1 + $2, 0)  }
-             | NUMBER ' ' am_or_pm            { $$ = hhmm($1 + $3, 0)  }
+time_in_HHMM : NUMBER ':' NUMBER              { $$ = hhmm24($1, $3) }
+             | NUMBER ':' NUMBER am_or_pm     { $$ = hhmm12($1, $3, $4) }
+             | NUMBER ':' NUMBER ' ' am_or_pm { $$ = hhmm12($1, $3, $5) }
+             | NUMBER am_or_pm                { $$ = hhmm12($1, 0,  $2) }
+             | NUMBER ' ' am_or_pm            { $$ = hhmm12($1, 0,  $3) }
              ;
 
 weekly_spec : WEEKLY AT time_in_HHMM ON day_name { $$ = weekly($3, $5) }
@@ -98,8 +100,8 @@ weekly_spec : WEEKLY AT time_in_HHMM ON day_name { $$ = weekly($3, $5) }
             | day_name    time_in_HHMM           { $$ = weekly($2, $1) }
             ;
 
-am_or_pm: AM { $$ = 0  }
-        | PM { $$ = 12 }
+am_or_pm: AM { $$ = true  }
+        | PM { $$ = false }
         ;
 
 day_name : SUNDAY     { $$ = time.Sunday    }

timespec/util.go
@@ -4,10 +4,16 @@ import (
 	"time"
 )
 
-func hhmm(hours uint, minutes uint) int {
-	for hours >= 24 {
-		hours -= 12
+func hhmm12(hours, minutes uint, am bool) int {
+	if am && hours == 12 { /* 12am is 00:00 */
+		hours = 0
+	} else if !am && hours > 12 { /* 12pm is 12:00 */
+		hours += 12
 	}
+	return hhmm24(hours, minutes)
+}
+
+func hhmm24(hours, minutes uint) int {
 	return int(hours*60 + minutes)
 }
 

core/v2.go
@@ -237,6 +237,23 @@ func (core *Core) v2API() *route.Router {
 		r.OK(users)
 	})
 	// }}}
+	r.Dispatch("POST /v2/ui/check/timespec", func(r *route.Request) { // {{{
+		var in struct {
+			Timespec string `json:"timespec"`
+		}
+		if !r.Payload(&in) {
+			return
+		}
+
+		spec, err := timespec.Parse(in.Timespec)
+		if err != nil {
+			r.Fail(route.Bad(err, fmt.Sprintf("%s", err)))
+			return
+		}
+
+		r.Success("%s", spec)
+	})
+	// }}}
 
 	r.Dispatch("GET /v2/auth/providers", func(r *route.Request) { // {{{
 		l := make([]AuthProviderConfig, 0)
@@ -1312,6 +1329,11 @@ func (core *Core) v2API() *route.Router {
 			return
 		}
 
+		if r.ParamIs("test", "t") {
+			r.Success("validation suceeded (request made in ?test=t mode)")
+			return
+		}
+
 		target, err := core.DB.CreateTarget(&db.Target{
 			TenantUUID: uuid.Parse(r.Args[1]),
 			Name:       in.Name,
@@ -1484,6 +1506,11 @@ func (core *Core) v2API() *route.Router {
 		}
 		if in.Expires%86400 != 0 {
 			r.Fail(route.Bad(nil, "Retention policy expiry must be a multiple of 1 day"))
+			return
+		}
+
+		if r.ParamIs("test", "t") {
+			r.Success("validation suceeded (request made in ?test=t mode)")
 			return
 		}
 
@@ -1676,6 +1703,11 @@ func (core *Core) v2API() *route.Router {
 		tenant, err := core.DB.GetTenant(r.Args[1])
 		if tenant == nil || err != nil {
 			r.Fail(route.Oops(err, "Unable to retrieve storage system information"))
+			return
+		}
+
+		if r.ParamIs("test", "t") {
+			r.Success("validation suceeded (request made in ?test=t mode)")
 			return
 		}
 

docs/API.yml
@@ -1751,6 +1751,15 @@ sections:
         access: [tenant, engineer]
 
         request:
+          query:
+            - name: test
+              type: bool
+              summary: |
+                Perform all validation and preparatory steps, but don't
+                actually create the target in the database.  This is useful
+                for validating that a target _could_ be created, without
+                creating it (i.e. for defering creation until later).
+
           json: |
             {
               "name"     : "New Target Name",
@@ -1769,6 +1778,10 @@ sections:
             JSON, inline, for both readability and sanity's sake.
 
             FIXME: Fix target.endpoint string -> JSON problem.
+
+            The following query string parameters are honored:
+
+            {{QUERY}}
 
         response:
           json: |
@@ -1991,6 +2004,15 @@ sections:
         access: [tenant, engineer]
 
         request:
+          query:
+            - name: test
+              type: bool
+              summary: |
+                Perform all validation and preparatory steps, but don't
+                actually create the store in the database.  This is useful
+                for validating that a store _could_ be created, without
+                creating it (i.e. for defering creation until later).
+
           json: |
             {
               "name"    : "Storage System Name",
@@ -2008,6 +2030,10 @@ sections:
             The values under `config` will depend entirely on which `plugin`
             has been selected; no validation will be done by the SHIELD Core,
             until the storage system is used in a job.
+
+            The following query string parameters are honored:
+
+            {{QUERY}}
 
         response:
           json: |
@@ -2198,6 +2224,15 @@ sections:
         access: [tenant, engineer]
 
         request:
+          query:
+            - name: test
+              type: bool
+              summary: |
+                Perform all validation and preparatory steps, but don't
+                actually create the policy in the database.  This is useful
+                for validating that a policy _could_ be created, without
+                creating it (i.e. for defering creation until later).
+
           json: |
             {
               "name"    : "Retention Policy Name",
@@ -2210,6 +2245,10 @@ sections:
             The `expires` value must be specified in seconds, and
             must be at least 86,400 (1 day) and be a multiple of
             86,400.
+
+            The following query string parameters are honored:
+
+            {{QUERY}}
 
         response:
           json: |

timespec/yacc
@@ -1,2 +1,4 @@
 #!/bin/sh
+go get     golang.org/x/tools/cmd/goyacc
+go install golang.org/x/tools/cmd/goyacc
 goyacc lang.y

commit 003f5765d4fe156cf1eb3488d8ca69498b2ecdf4

Fix 12h time detection in timespec
Regression introduced in d4ba545

timespec/lang.y
@@ -59,15 +59,15 @@ spec : hourly_spec | daily_spec | weekly_spec | monthly_spec
 
 hourly_spec : HOURLY     AT time_in_MM             { $$ = hourly($3, 0) }
             | HOURLY        time_in_MM             { $$ = hourly($2, 0) }
-            | EVERY QUARTER HOUR FROM time_in_MM     { $$ = hourly($5, 0.25) }
-            | EVERY HALF HOUR FROM time_in_MM        { $$ = hourly($5, 0.5) }
+            | EVERY QUARTER HOUR FROM time_in_MM   { $$ = hourly($5, 0.25) }
+            | EVERY HALF HOUR FROM time_in_MM      { $$ = hourly($5, 0.5) }
             | EVERY HOUR AT time_in_MM             { $$ = hourly($4, 0) }
             | EVERY HOUR time_in_MM                { $$ = hourly($3, 0) }
             | EVERY NUMBER HOUR FROM time_in_HHMM  { $$ = hourly($5, float32($2)) }
             ;
 
-daily_spec : DAILY    AT time_in_HHMM  { $$ = daily($3) }
-           | DAILY       time_in_HHMM  { $$ = daily($2) }
+daily_spec : DAILY    AT time_in_HHMM   { $$ = daily($3) }
+           | DAILY       time_in_HHMM   { $$ = daily($2) }
            | EVERY DAY AT time_in_HHMM  { $$ = daily($4) }
            | EVERY DAY    time_in_HHMM  { $$ = daily($3) }
            ;

timespec/util.go
@@ -7,7 +7,7 @@ import (
 func hhmm12(hours, minutes uint, am bool) int {
 	if am && hours == 12 { /* 12am is 00:00 */
 		hours = 0
-	} else if !am && hours > 12 { /* 12pm is 12:00 */
+	} else if !am && hours < 12 { /* 12pm is 12:00 */
 		hours += 12
 	}
 	return hhmm24(hours, minutes)