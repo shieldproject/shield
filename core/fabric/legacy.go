c89c47442c12a29b50f0761c0a078ddece5308de
James Hunt <james@niftylogic.com>
2019-02-04 11:30:01 -0500

    Log to standard error in legacy fabric
    The Infof() call was getting sent to stdout, causing serious JSON
    deserialization and parsing issues.

--- core/fabric/legacy.go
+++ core/fabric/legacy.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"encoding/json"
 
-	"golang.org/x/crypto/ssh"
 	"github.com/jhunt/go-log"
+	"golang.org/x/crypto/ssh"
 
 	"github.com/starkandwayne/shield/core/scheduler"
 	"github.com/starkandwayne/shield/core/vault"
@@ -125,7 +125,7 @@ func (f LegacyFabric) Execute(op, id string, command Command) scheduler.Chore {
 			}
 			payload := string(b)
 
-			chore.Infof("connecing to %s (tcp/ipv4)", f.ip)
+			chore.Errorf("connecing to %s (tcp/ipv4)", f.ip)
 			conn, err := ssh.Dial("tcp4", f.ip, f.ssh)
 			if err != nil {
 				chore.Errorf("ERR> unable to connect to %s: %s", err)
