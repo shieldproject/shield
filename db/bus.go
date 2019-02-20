4b85807c9a70d2b3dd658c66e96efa0d10cd4940
James Hunt <james@niftylogic.com>
2019-02-20 12:45:53 -0500

    Improve logging in db->mbus handoffs
    It's difficult to trace down a data-driven task without a pointer to the
    data thing that's causing the crash.  Now we have such pointers.

--- db/bus.go
+++ db/bus.go
@@ -4,23 +4,68 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/jhunt/go-log"
+
 	"github.com/starkandwayne/shield/core/bus"
 )
+
+func datauuid(thing interface{}) string {
+	switch thing.(type) {
+	case Agent:
+		return fmt.Sprintf("agent [%s]", thing.(Agent).UUID)
+	case *Agent:
+		return fmt.Sprintf("agent [%s]", thing.(*Agent).UUID)
+
+	case Job:
+		return fmt.Sprintf("job [%s]", thing.(Job).UUID)
+	case *Job:
+		return fmt.Sprintf("job [%s]", thing.(*Job).UUID)
+
+	case Store:
+		return fmt.Sprintf("store [%s]", thing.(Store).UUID)
+	case *Store:
+		return fmt.Sprintf("store [%s]", thing.(*Store).UUID)
+
+	case Target:
+		return fmt.Sprintf("target [%s]", thing.(Target).UUID)
+	case *Target:
+		return fmt.Sprintf("target [%s]", thing.(*Target).UUID)
+
+	case Tenant:
+		return fmt.Sprintf("tenant [%s]", thing.(Tenant).UUID)
+	case *Tenant:
+		return fmt.Sprintf("tenant [%s]", thing.(*Tenant).UUID)
+
+	case Task:
+		return fmt.Sprintf("task [%s]", thing.(Task).UUID)
+	case *Task:
+		return fmt.Sprintf("task [%s]", thing.(*Task).UUID)
+
+	default:
+		panic("SHIELD was unable to determine the type of thing, in order to craft a message bus event for it.  This is most certainly a bug in SHIELD itself.")
+	}
+}
 
 func datatype(thing interface{}) string {
 	switch thing.(type) {
 	case Agent, *Agent:
 		return "agent"
+
 	case Job, *Job:
 		return "job"
+
 	case Store, *Store:
 		return "store"
+
 	case Target, *Target:
 		return "target"
+
 	case Tenant, *Tenant:
 		return "tenant"
+
 	case Task, *Task:
 		return "task"
+
 	default:
 		panic("SHIELD was unable to determine the type of thing, in order to craft a message bus event for it.  This is most certainly a bug in SHIELD itself.")
 	}
@@ -28,25 +73,28 @@ func datatype(thing interface{}) string {
 
 func (db *DB) sendCreateObjectEvent(thing interface{}, queues ...string) {
 	if db.bus != nil {
-		fmt.Printf("sending %s to [%s] for %s\n", bus.CreateObjectEvent, strings.Join(queues, ", "), datatype(thing))
+		log.Infof("sending %s to [%s] for %s", bus.CreateObjectEvent, strings.Join(queues, ", "), datauuid(thing))
 		db.bus.Send(bus.CreateObjectEvent, datatype(thing), thing, queues...)
 	}
 }
 
 func (db *DB) sendUpdateObjectEvent(thing interface{}, queues ...string) {
 	if db.bus != nil {
+		log.Infof("sending %s to [%s] for %s", bus.UpdateObjectEvent, strings.Join(queues, ", "), datauuid(thing))
 		db.bus.Send(bus.UpdateObjectEvent, datatype(thing), thing, queues...)
 	}
 }
 
 func (db *DB) sendDeleteObjectEvent(thing interface{}, queues ...string) {
 	if db.bus != nil {
+		log.Infof("sending %s to [%s] for %s", bus.DeleteObjectEvent, strings.Join(queues, ", "), datauuid(thing))
 		db.bus.Send(bus.DeleteObjectEvent, datatype(thing), thing, queues...)
 	}
 }
 
 func (db *DB) sendTaskStatusUpdateEvent(task *Task, queues ...string) {
 	if db.bus != nil {
+		log.Infof("sending %s to [%s] for task [%s]", bus.TaskStatusUpdateEvent, strings.Join(queues, ", "), task.UUID)
 		db.bus.Send(bus.TaskStatusUpdateEvent, "", map[string]interface{}{
 			"uuid":       task.UUID,
 			"status":     task.Status,
@@ -57,11 +105,12 @@ func (db *DB) sendTaskStatusUpdateEvent(task *Task, queues ...string) {
 	}
 }
 
-func (db *DB) sendTaskLogUpdateEvent(id, log string, queues ...string) {
+func (db *DB) sendTaskLogUpdateEvent(id, msg string, queues ...string) {
 	if db.bus != nil {
+		log.Infof("sending %s to [%s] for task [%s]", bus.TaskLogUpdateEvent, strings.Join(queues, ", "), id)
 		db.bus.Send(bus.TaskLogUpdateEvent, "", map[string]interface{}{
 			"uuid": id,
-			"tail": log,
+			"tail": msg,
 		}, queues...)
 	}
 }
