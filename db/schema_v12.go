da6466d28f69ead089e963e04265cccc7d6ca0da
Sriniketh Varma <srinikethvarma22@gmail.com>
2019-11-26 14:26:27 -0500

    Fixed code duplication issues

--- db/schema_v12.go
+++ db/schema_v12.go
@@ -7,45 +7,6 @@ type v12Schema struct{}
 func (s v12Schema) Deploy(db *DB) error {
 	var err error
 
-	// set the tenant_uuid column to NOT NULL
-	err = db.Exec(`CREATE TABLE jobs_new (
-                    uuid               UUID PRIMARY KEY,
-                    target_uuid        UUID NOT NULL,
-                    store_uuid         UUID NOT NULL,
-                    tenant_uuid        UUID NOT NULL,
-                    name               TEXT NOT NULL,
-                    summary            TEXT NOT NULL,
-                    schedule           TEXT NOT NULL,
-                    keep_n             INTEGER NOT NULL DEFAULT 0,
-                    keep_days          INTEGER NOT NULL DEFAULT 0,
-                    next_run           INTEGER DEFAULT 0,
-                    priority           INTEGER DEFAULT 50,
-                    paused             BOOLEAN NOT NULL DEFAULT 0,
-                    fixed_key          INTEGER DEFAULT 0,
-                    healthy            BOOLEAN NOT NULL DEFAULT 0
-                )`)
-	if err != nil {
-		return err
-	}
-	err = db.Exec(`INSERT INTO jobs_new (uuid, target_uuid, store_uuid, tenant_uuid,
-                            name, summary, schedule, keep_n, keep_days,
-                            next_run, priority, paused, fixed_key, healthy)
-                    SELECT j.uuid, j.target_uuid, j.store_uuid, j.tenant_uuid,
-                            j.name, j.summary, j.schedule, j.keep_n, j.keep_days,
-                            j.next_run, j.priority, IFNULL(j.paused, 0), j.fixed_key, 0
-                        FROM jobs j`)
-	if err != nil {
-		return err
-	}
-	err = db.Exec(`DROP TABLE jobs`)
-	if err != nil {
-		return err
-	}
-	err = db.Exec(`ALTER TABLE jobs_new RENAME TO jobs`)
-	if err != nil {
-		return err
-	}
-
 	jobs, err := db.GetAllJobs(&JobFilter{})
 	if err != nil {
 		return nil
