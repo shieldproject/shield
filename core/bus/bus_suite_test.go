2b8640bc198c6ccf7f67713c2b2384c731151f06
Wayne E. Seguin <wayneeseguin@gmail.com>
2026-03-27 16:02:03 -0400

    Bootstrap Ginkgo test suites for untested packages
    Add suite bootstrap files for route, core/scheduler,
    and core/bus packages to enable BDD testing with
    Ginkgo v1 + Gomega.

---
+++ core/bus/bus_suite_test.go
@@ -0,0 +1,12 @@
+package bus_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestBus(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Bus Suite")
+}
